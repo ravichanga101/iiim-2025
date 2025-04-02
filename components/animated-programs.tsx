6a8486da820c5db91c65f037fa8771d358c06cb7
Ravi Patel <ravifromchanga@gmail.com>
2025-04-02 15:30:26 +0530

    programs updated

--- components/animated-programs.tsx
+++ components/animated-programs.tsx
@@ -39,7 +39,7 @@ export function AnimatedPrograms() {
           "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?height=300&width=500",
         features: [
           { icon: Clock, text: "3/4 Years" },
-          { icon: Calendar, text: "June/July Intake" },
+          { icon: Calendar, text: "June / July" },
           { icon: Award, text: "UGC/AICTE" },
           { icon: IndianRupee, text: "₹1,00,000 / year" },
         ],
