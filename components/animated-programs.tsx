a33680c5e03dc70cda20f3856500060410e2c18a
Ravi Patel <ravifromchanga@gmail.com>
2025-04-03 15:00:14 +0530

    remove intake

--- components/animated-programs.tsx
+++ components/animated-programs.tsx
@@ -39,8 +39,8 @@ export function AnimatedPrograms() {
           "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?height=300&width=500",
         features: [
           { icon: Clock, text: "3/4 Years" },
-          { icon: Calendar, text: "June / July Intake" },
-          { icon: Award, text: "UGC/AICTE Approved" },
+          // { icon: Calendar, text: "June / July Intake" },
+          { icon: Award, text: "UGC / AICTE Approved" },
           { icon: IndianRupee, text: "1,00,000 / year" },
         ],
       },
@@ -54,7 +54,7 @@ export function AnimatedPrograms() {
           "https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?height=300&width=500?height=300&width=500",
         features: [
           { icon: Clock, text: "2 Years" },
-          { icon: Calendar, text: "July / August Intake" },
+          // { icon: Calendar, text: "July / August Intake" },
           { icon: Award, text: "UGC Approved" },
           { icon: IndianRupee, text: "1,45,000 / year" },
         ],
@@ -70,7 +70,7 @@ export function AnimatedPrograms() {
           "https://images.unsplash.com/photo-1499750310107-5fef28a66643?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?height=300&width=500",
         features: [
           { icon: Clock, text: "2 Years" },
-          { icon: Calendar, text: "July / August Intake" },
+          // { icon: Calendar, text: "July / August Intake" },
           { icon: Award, text: "UGC Approved" },
           { icon: IndianRupee, text: "1,45,000 / year" },
         ],
