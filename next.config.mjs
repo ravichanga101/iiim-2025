032534d2007f83246cffb2f748aa24b54e25c840
Ravi Patel <ravifromchanga@gmail.com>
2025-04-03 20:44:53 +0530

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
   },
   output: "export", // Required for static exports
   // basePath: isProd ? `/${repoName}` : "",
-  bashPath: "https://www.charusat.ac.in/iiim/",
+  bashPath: "/iiim",
   assetPrefix: "https://www.charusat.ac.in/iiim/",
   images: {
     unoptimized: true,
